16788cbfe595e50eefd5e638cfb67fbb86093252
fryholic <140505972+fryholic@users.noreply.github.com>
2024-12-20 20:26:49 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string A, B, C, D;
+    A = "\\    /\\";
+    B = " )  ( ')";
+    C = "(  /  )";
+    D = " \\(__)|";
+    cout << A << endl << B << endl << C << endl << D;
+    return 0;
+}
