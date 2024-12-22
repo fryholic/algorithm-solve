7a502cd7e78049d4b2c12de857ee21f6e7c9092d
fryholic <140505972+fryholic@users.noreply.github.com>
2024-12-23 02:43:14 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    //cin.tie(NULL);
+    //ios::sync_with_stdio(false);
+    
+    char c;
+
+    while(scanf("%c", &c) != EOF){
+        printf("%c", c);
+    }
+
+    return 0;
+}
