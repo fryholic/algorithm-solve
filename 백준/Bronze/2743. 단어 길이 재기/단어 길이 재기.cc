7f3a4162fd2856738fb0ca2aba1b2dd5319f83bd
fryholic <140505972+fryholic@users.noreply.github.com>
2024-12-23 00:47:23 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    char A[101];
+    cin >> A;
+    int len = strlen(A);
+    cout << len;
+
+    return 0;
+}
