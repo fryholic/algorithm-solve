650c5e6182efd23a3c4a2e7ad6e99ca767cff7c9
fryholic <140505972+fryholic@users.noreply.github.com>
2025-01-21 02:05:53 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,76 +1,70 @@
 #include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
 #include <algorithm>
+
 using namespace std;
- 
-// 비교할 체스판 문양 2개를 미리 string 배열로 생성
-string WB[8] = {
-    "WBWBWBWB",
-    "BWBWBWBW",
-    "WBWBWBWB",
-    "BWBWBWBW",
-    "WBWBWBWB",
-    "BWBWBWBW",
-    "WBWBWBWB",
-    "BWBWBWBW"
-};
-string BW[8] = {
-    "BWBWBWBW",
-    "WBWBWBWB",
-    "BWBWBWBW",
-    "WBWBWBWB",
-    "BWBWBWBW",
-    "WBWBWBWB",
-    "BWBWBWBW",
-    "WBWBWBWB"
-};
- 
-// 입력받은 m*n 보드를 받기 위한 배열
-string board[50];
- 
-int cntWB(int x, int y);
-int cntBW(int x, int y);
- 
-int main(){
-    ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
-    cin.tie(NULL);	// 입력과 출력 묶음을 풀기
- 
-    int n, m;
-    cin >> n >> m;
-    cin.ignore();
- 
-    for(int i=0; i<n; i++){
-        getline(cin, board[i]);
+
+
+
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+        
+    int N, M;
+    cin >> N >> M;
+
+    string board[50];
+
+    for (int i = 0; i < N; i++) {
+        cin >> board[i];
     }
-    int minVal = 65; // 8*8 칸 모두 다시 칠해도 64개
+
+    int ans = 65;
+
+    string temp1[8] = { "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW" };
+    string temp2[8] = { "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB" };
+
     
-    for(int i=0; i+8 <= n; i++){
-        for(int j=0; j+8 <= m; j++){
-            int tmp = min(cntWB(i, j), cntBW(i, j));
-            if(tmp < minVal) minVal = tmp;
+
+    for (int i = 0; i < N - 7; i++) {
+        for (int j = 0; j < M - 7; j++) {
+
+            int temp1_count = 0;
+            int temp2_count = 0;
+
+            for (int k = 0; k < 8; k++) {
+                for (int l = 0; l < 8; l++) {
+                    if (board[i + k][j + l] != temp1[k][l]) {
+                        temp1_count++;
+                    }
+
+                    if (board[i + k][j + l] != temp2[k][l]) {
+                        temp2_count++;
+                    }
+                }
+            }
+            
+                if (temp1_count > temp2_count) {
+                    if (temp2_count <= ans) {
+                        ans = temp2_count;
+                    }
+                }
+                else {
+                    if (temp1_count <= ans) {
+                        ans = temp1_count;
+                    }
+                }
+            
         }
     }
-    cout << minVal;
+
+    
+    
+    cout << ans;
+
     return 0;
-}
- 
-// W로 시작하는 체스판: 새로 칠해야 하는 칸 수 반환
-int cntWB(int x, int y){
-    int cnt = 0;
-    for(int i=0; i<8; i++){
-        for(int j=0; j<8; j++){
-            if(board[x+i][y+j] != WB[i][j]) cnt++;
-        }
-    }
-    return cnt;
-}
- 
-// B로 시작하는 체스판: 새로 칠해야 하는 칸 수 반환
-int cntBW(int x, int y){
-    int cnt = 0;
-    for(int i=0; i<8; i++){
-        for(int j=0; j<8; j++){
-            if(board[x+i][y+j] != BW[i][j]) cnt++;
-        }
-    }
-    return cnt;
+
 }
