cbe1726103506c60e39ba5b4801a4c5e58367372
fryholic <140505972+fryholic@users.noreply.github.com>
2025-05-15 13:08:23 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -1,34 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <deque>
 #include <algorithm>
 #include <stack>
-#include <cstring>
 #include <string>
-#include <functional>
-#include <numeric>
 #include <cmath>
-
 
 using namespace std;
 
 int quad(int N, int r, int c) {
 	int size = pow(2, N);
-
-	if (r < size / 2) {
-		if (c < size / 2) { // 2사분면 : 1
+    if (r < size / 2) {
+		if (c < size / 2) { // 2사분면
 			return 1;
 		}
-		else { // 1사분면 : 2
+		else { // 1사분면
 			return 2;
 		}
 	}
 	else {
-		if (c < size / 2) { // 3사분면 : 3
+		if (c < size / 2) { // 3사분면
 			return 3;
 		}
-		else { // 4사분면 : 4
+		else { // 4사분면
 			return 4;
 		}
 	}
@@ -44,31 +38,29 @@ int main() {
   int seg = N;
   
   int flag = 0;
-  while (seg) {
-	  seg--;
-	  int size = pow(2, N);
-	  int quad_n = quad(N, r, c);
-	  if (quad_n == 1) { // 2사분면
-		  flag += 0;
-	  }
-	  else if (quad_n == 2) { // 1사분면
-		  flag += pow(2, 2 * seg);;
-		  c -= size / 2;
-	  }
-	  else if (quad_n == 3) { // 3사분면
-		  flag += pow(2, 2 * seg + 1);
-		  r -= size / 2;
-	  }
-	  else if (quad_n == 4) { // 4사분면
-		  flag += pow(2, 2 * seg + 1) + pow(2, 2 * seg);
-		  c -= size / 2;
-		  r -= size / 2;
-	  }
-	  N--;
-  }
+  while (N) {
+       N--;
+       int size = pow(2, N + 1);
+       int quad_n = quad(N + 1, r, c);
+       if (quad_n == 1) { // 2사분면
+           flag += 0;
+       }
+       else if (quad_n == 2) { // 1사분면
+           flag += pow(2, 2 * N);
+           c -= size / 2;
+       }
+       else if (quad_n == 3) { // 3사분면
+           flag += pow(2, 2 * N + 1);
+           r -= size / 2;
+       }
+       else if (quad_n == 4) { // 4사분면
+           flag += pow(2, 2 * N + 1) + pow(2, 2 * N);
+           c -= size / 2;
+           r -= size / 2;
+       }
+   }
   
   cout << flag << '\n';
-
 
   return 0;
 }
